35fd9abcc51d9b678acb9677ef841a7088968660
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2025-02-13 14:40:51 +0100

    update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ server.use(morgan("dev"));
 // Middleware to disable CORS
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
